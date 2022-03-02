8493002789345bf3389d00270d469efd755227e5
Vansh Wassan <007blackhacker@gmail.com>
2022-03-02 20:47:32 +0530

    /getUser updates + error handling

--- api/routes/getUser.js
+++ api/routes/getUser.js
@@ -4,13 +4,36 @@ const Users = require('../models/Users');
 
 router.get('/', async (req, res) => {
     try {
-        const ethAddress = req.query.ethAddress;
+        const ethAddress = req.header('sponsorAddress');
+        const chainId = req.header('chainId');
         const User = await Users.findOne({ethAddress: ethAddress});
-        res.json(User);
-        console.log("User with ETH Address Found in DB");
-        console.log(User);
+
+        if (chainId == 1){
+            res.status(500)
+            res.json("Please select a different chainID");
+        }
+        else {
+
+            if (ethAddress) {
+                if (User) {
+                    res.json(User);
+                    console.log(User);
+                }
+                else if (User == null) {
+                    res.json({message: "No User Found for that ETH Address"});
+                    console.log("No User Found for that ETH Address");
+                }
+                res.json(User);
+            }
+            else if (!ethAddress) {
+                res.status(500)
+                res.json({message: "Please supply a valid ETH Address"});
+                console.log("Please supply a valid ETH Address");
+            }
+    }
     } catch (err) {
-        res.json({message: err});
+        res.json({message: "Error in Getting User from DB"});
+        console.log(err);
     }
 });
 
