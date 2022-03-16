7c3857756eb935affb5392bf87f688a3e8e913fb
Vansh Wassan <007blackhacker@gmail.com>
2022-03-16 19:34:29 +0530

    resolved issues for pr

--- api/app.js
+++ api/app.js
@@ -4,35 +4,38 @@ const mongoose = require('mongoose');
 require('dotenv/config');
 const bodyParser = require('body-parser');
 const Users = require('./models/Users');
-// const API_KEY = "XdONGxOjq82pZrewrlUM";
 const cors = require('cors');
-
 app.use(bodyParser.json());
 app.use(cors());
 
-//ROUTES
-
-// POST Route to create a new user
+// POST Route to Create a New User
 app.post("/createUser", async (req, res) => {
+    try {
     const user = new Users({
       name: req.body.name,
       ethAddress: req.body.ethAddress,
       companyName: req.body.companyName,
     });
-    const apiKey = req.header("x-api-key"); //gives you the value of the header
-    if (apiKey != process.env.API_KEY) {
-        console.log("x-api-key authentication failed");
-        return res.json({message: "Incorrect x-api-key."});
+    const apiKey = req.header("x-api-key");
+
+    if (apiKey != process.env.API_KEY) throw "Incorrect x-api-key. Authentication failed.";
+
+    if (!user.name || !user.ethAddress || !user.companyName) throw "Please supply a valid name, ethAddress, and companyName.";
+
+        else {
+            try{
+            const savedUser = await user.save();
+            console.log("User Added!");
+            return res.json(savedUser);
+            }
+            catch (err) {
+                console.log("A User with that ETH Address already exists!");
+                return res.status(500).json({ message: "A User with that ETH Address already exists!" });
+            }
         }
-    if (!user.name || !user.ethAddress || !user.companyName) {
-        return res.status(400).json({ message: "Please supply a valid name, ethAddress, and companyName" });
-    }
-    try {
-      const savedUser = await user.save();
-      console.log("User Added!");
-      return res.json(savedUser);
-    } catch (err) {
-      return res.status(500).json({ message: "A User with that ETH Address already exists" });
+    }   catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: err });
     }
   });
 
@@ -40,34 +43,32 @@ app.post("/createUser", async (req, res) => {
 // GET Route to get a specific user from his ETH Address
 app.get('/getUser', async (req, res) => {
     try {
-        const apiKey = req.header("x-api-key"); //gives you the value of the header
+        const apiKey = req.header("x-api-key");
         const ethAddress = req.header('sponsorAddress');
         const chainId = req.header('chainId');
         const User = await Users.findOne({ ethAddress });
 
         if (apiKey != process.env.API_KEY) throw "Incorrect x-api-key. Authentication failed.";
 
-        if (chainId == 1) throw "No Mainnet. Please select a different chainID";
+        if (chainId == 1) throw "No requests on Mainnet. Please select a different chainID";
 
         if (!ethAddress) throw "Please supply a valid ETH Address."
 
         if (!User) throw "No user found for that ETH address.";
-            //return User after all guard clauses passed
+
             else {
                 console.log(User);
                 return res.json(User);
             }
     
-    } catch (err) { // Have the API return any errors thrown.
-        res.status(500).json({ message: err });
-        // if (bug) throw "Bug found";
+    } catch (err) {
         console.log(err);
-        return res.json({message: "Error in Getting User from DB"});
+        return res.status(500).json({ message: err });
     }
 });
-    //Connect to DB
+
     mongoose.connect(process.env.DB_CONNECT, () => {
-        console.log('Connected to DB');
+        console.log('API listening on port 3000');
     });
 
 app.listen(3000);
--- api/models/Users.js
+++ api/models/Users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator'); 
 
-const userSchema = mongoose.Schema({ //noSQL schema, easier than SQL; it describes the way your data looks
+const userSchema = mongoose.Schema({
     name: {
         type: String,
         required: true
@@ -17,4 +17,4 @@ const userSchema = mongoose.Schema({ //noSQL schema, easier than SQL; it describ
     }
 });
 userSchema.plugin(uniqueValidator)
-module.exports = mongoose.model('Users', userSchema); //give it name and schema to use
+module.exports = mongoose.model('Users', userSchema);
